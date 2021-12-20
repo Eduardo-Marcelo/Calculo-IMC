025baeca74876832f24fc15909e4539912fd323b
Eduardo Marcelo <duduprogrammer@gmail.com>
2021-12-20 19:06:06 -0300

    Calculando o IMC e guardando o resultado em uma variável

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ print('Para começar precisamos de algumas informações')
 peso = int(input('Informe seu peso: '))
 
 altura = float(input("Informe sua altura: "))
+
+resultado = peso / (altura * altura)
+
