701ead741c8537008d60ac24bfa14f4b2575a496
Eduardo Marcelo <duduprogrammer@gmail.com>
2021-12-20 19:14:49 -0300

    Verificação do  estado do usuário com a mensagem de resultado final

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ altura = float(input("Informe sua altura: "))
 
 resultado = peso / (altura * altura)
 
+if resultado < 18.5:
+    print('Abaixo do peso')
+elif resultado > 18.5 and resultado < 24.9:
+    print('Peso ideal')
+elif resultado > 25 and resultado < 29.9:
+    print('Sobrepeso')
+elif resultado > 30 and resultado < 39.9:
+    print('Obeso')
+elif resultado > 40:
+    print('Obeso Mórbido')            
